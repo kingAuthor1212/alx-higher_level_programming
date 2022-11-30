9b6b678c513bdcc1f90eb50bed28452b8dd92861
kingAutgor1212 <kingauthor1212@gmail.com>
2022-11-29 23:51:11 -0800

    Pycodestyle

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,6 +3,13 @@
 
 #include <stdlib.h>
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct listint_s
 {
 	int n;
@@ -11,7 +18,7 @@ typedef struct listint_s
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(listint_t  * head);
+void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif 
