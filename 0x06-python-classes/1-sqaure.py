efc7c025bc3a7c7a103acb8c6c9109ff92745d58
kingAutgor1212 <kingauthor1212@gmail.com>
2022-12-21 12:54:24 -0800

    1. Square with size

--- 0x06-python-classes/1-sqaure.py
+++ 0x06-python-classes/1-sqaure.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # 1-square.py
-
 """Define a class Square."""
 
 
@@ -12,4 +11,4 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-        self.__size = size 
+        self.__size = size
