311862c331c2d1d64c4c16fcff4a8ceb3f0fac98
kingAutgor1212 <kingauthor1212@gmail.com>
2023-03-13 12:14:41 -0700

    0x12. JavaScript - Warm up*

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const argv = process.argv;
-if (argv[2] == undefined) {
-	console.log('No argument');
+
+if (process.argv[2]) {
+  console.log(process.argv[2]);
 } else {
-	console.log(argv[2]);
+  console.log('No argument');
 }
