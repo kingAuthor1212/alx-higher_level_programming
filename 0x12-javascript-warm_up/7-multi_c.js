bb85de1757ca0daff43da39c730dbba45f57514b
kingAutgor1212 <kingauthor1212@gmail.com>
2023-03-13 13:07:44 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const num = parseInt(process.argv[2]);
+const limit = parseInt(process.argv[2]);
 
-if (num) {
-	for (let i = 0; i < num; ++i) {
-		console.log('C is fun');
-	} else {
-		console.log('Missing number of occurrences');
-	}
+if (limit) {
+  for (let i = 0; i < limit; ++i) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
