9708f6db00942c0aea5bfdbe9b09697d1c23f26e
kingAutgor1212 <kingauthor1212@gmail.com>
2023-03-15 00:02:44 -0700

    0x13-javascript_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const OldSquare = require('./5-square');
+
+module.exports = class Square extends OldSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  double () {
+    super.double();
+  }
+
+  charPrint (c = 'X') {
+    super.print(c);
+  }
+};
