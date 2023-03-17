54707f815f6261f14d9dd7dcd0672c4e50439626
kingAutgor1212 <kingauthor1212@gmail.com>
2023-03-17 01:51:50 -0700

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            usr=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3360,
+            host='localhost'
+            )
+
+    cursor = db.cursor()
+    cursor.execute(
+        'SELECT cities.name FROM cities\
+        INNER JOIN states ON cities.state_id = states.id\
+        WHERE states.name = %s \
+        ORDER BY cities.id ASC', (sys.argv[4], ))
+
+    cities = cursor.fetchall()
+
+    idx = 0
+    for city in cities:
+        if idx != 0:
+            print(", ", end="")
+        print("%s" % city, end="")
+        idx += 1
+    print("")
+
+    cursor.close()
+    db.close()
