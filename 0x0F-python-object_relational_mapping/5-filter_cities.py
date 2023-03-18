dfcccf3131691af32249d533dbe5fc2c724303c8
kingAutgor1212 <kingauthor1212@gmail.com>
2023-03-18 12:39:18 -0700

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,41 +1,22 @@
 #!/usr/bin/python3
-"""
-This script  takes in the name of a state
-as an argument and lists all cities of that
-state, using the database `hbtn_0e_4_usa`.
-"""
+'''
+Lists all cities from the database hbtn_0e_4_usa.
+'''
 
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-    """
-    Access to the database and get the cities
-    from the database.
-    """
+if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
 
-    with db.cursor() as cur:
-        cur.execute("""
-            SELECT
-                cities.id, cities.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
-            WHERE
-                states.name LIKE BINARY %(state_name)s
-            ORDER BY
-                cities.id ASC
-        """, {
-            'state_name': argv[4]
-        })
-
-        rows = cur.fetchall()
-
-    if rows is not None:
-        print(", ".join([row[1] for row in rows]))/
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN\
+            states ON cities.state_id = states.id WHERE states.name=%s\
+            ORDER by cities.id ASC;",
+                (argv[4], ))
+    query_rows = cur.fetchall()
+    print(', '.join([row[1] for row in query_rows]))
+    cur.close()
+    conn.close()
