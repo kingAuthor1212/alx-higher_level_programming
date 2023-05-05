dc0b4468b0f920709251ce826e8eaeff327e293a
kingAuthor1212 <kingauthor1212@gmail.com>
2023-05-05 02:05:24 -0700

    0x15-javascript-web_jquery

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,1 +1,1 @@
-$('document').ready(function () const url =  { $('')})
+$('document').ready(function () { $('INPUT#btn_translate').click(function () {$.get('https://www.fourtonfish.com/hellosalut/hello/' + $.param({lang: $('INPUT#language_code').val()}), function (data) {$('DIV#hello').html(data.hello);});});});
